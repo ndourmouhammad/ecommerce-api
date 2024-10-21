75a89b9c61e4c7e531103cb10d9e996c1dbae3a3
Mouhammad ndour <ndourmouhammad15@gmail.com>
2024-10-21 07:19:54 +0000

    Ajout des dependances

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
-use Illuminate\Http\Request;
     
 
 
